8b3db2c2f61cb45ae9e0be679889bdfc89060c02
Vaishu-sunkad <nagammasunkad46@gmail.com>
2025-04-16 12:01:58 +0530

    2nd commit

--- jenkinsdemo/jenkinDemo/src/student.java
+++ jenkinsdemo/jenkinDemo/src/student.java
@@ -5,6 +5,7 @@ class course{
 public class student {
     public static void main(String[] args) {
         course c=new course();
+        System.out.println("Student Details");
         System.out.println("     Student "+c.name+" choose "+ c.course);
         
     }
