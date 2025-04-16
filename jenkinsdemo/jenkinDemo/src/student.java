796dd3cd60453bdb5ed9a54bb2b6fe34b249e89e
Vaishu-sunkad <nagammasunkad46@gmail.com>
2025-04-16 11:38:29 +0530

    student class

---
+++ jenkinsdemo/jenkinDemo/src/student.java
@@ -0,0 +1,11 @@
+class course{
+    String name="Xyz";
+    String course="DevOps";
+}
+public class student {
+    public static void main(String[] args) {
+        course c=new course();
+        System.out.println("     Student "+c.name+" choose "+ c.course);
+        
+    }
+}
