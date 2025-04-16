b777f1eefb4529c65d20def2ff6e30b015e29a4a
Vaishu-sunkad <nagammasunkad46@gmail.com>
2025-04-16 11:31:24 +0530

    employee class

---
+++ jenkinsdemo/jenkinDemo/src/Employee.java
@@ -0,0 +1,12 @@
+class EmpDetails{
+    String name="Abc";
+    int salary=100000;
+}
+public class Employee {
+   public static void main(String[] args) {
+    EmpDetails emp=new EmpDetails();
+    System.out.println(emp.name);
+    System.out.println(emp.salary);
+
+   }
+}
